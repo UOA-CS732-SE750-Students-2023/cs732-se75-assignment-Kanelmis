51d97c12ec168e538ca98f4391affa7944752e54
Kanelmis <kanelmis@gmail.com>
2023-04-03 21:11:15 +1200

    Set Up React Project with Libraries Nivo, Mui..

---
+++ tech-demo/src/App.js
@@ -0,0 +1,23 @@
+import { ColourModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+
+function App() {
+  const [theme, colourMode] = useMode();
+  return (
+    <ColourModeContext.Provider value = {colourMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className = "content">
+            <Topbar />
+          </main>
+    
+        </div>
+    </ThemeProvider>
+    </ColourModeContext.Provider>
+    
+  );
+}
+
+export default App;
